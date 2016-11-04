764a75cb01840a41d537cf7daaf8932b1df4c398
Benson Mwaura <benson.cmwaura@gmail.com>
2016-11-04 16:08:53 +0300

    Update umati.js
    - updated

--- js/umati.js
+++ js/umati.js
@@ -1,22 +1,5 @@
 
 $(function(){
-  var signup_f = $("#registerForm"),
-      sf_input = $("#registerForm :input"),
-      reg_btn = $("#regbtn"),
-      reg = $("#regbtn"),
-      fname = $("#fname"),
-      lname = $("#lname"),
-      email = $("#email"),
-      pass = $("#passwd");
-  function registerUser(){
-    $.post(signup_f.attr("action"), sf_input.serializeArray(), function(){
-
-    });
-  };
-  reg_btn.click(function (evt) {
-    registerUser();
-    evt.preventDefault();
-  });
   $('.team-carousel').css('opacity', 0);
   $('.team-carousel').waypoint(function(direction == 'down'){
     if(direction == 'down'){
